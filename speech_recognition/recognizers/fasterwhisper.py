0a1686ce7ee3e6a7595980c6d6de661de2acdafe
Michael <hazardu5@gmail.com>
2023-10-07 14:51:11 +1300

    Improve STT logging
    - Add better profiling durations for time to init CUDA dll

--- speech_recognition/recognizers/fasterwhisper.py
+++ speech_recognition/recognizers/fasterwhisper.py
@@ -4,9 +4,12 @@ import os
 import time
 from io import BytesIO
 
+import numpy as np
+import soundfile as sf
+import faster_whisper
+
 from speech_recognition.audio import AudioData
 from speech_recognition.exceptions import SetupError
-
 
 def recognize_faster_whisper(
     recognizer,
@@ -31,13 +34,21 @@ def recognize_faster_whisper(
     The return dict is also customized to avoid returning a mess of FasterWhisper objects.
     """
 
+    
+
+
     assert isinstance(audio_data, AudioData), "Data must be audio data"
 
     import multiprocessing as mp
     queue = mp.Queue(maxsize=1)
     proc = mp.Process(target=perform_recognize_faster_whisper, args=(queue, recognizer, audio_data), kwargs=kwargs)
+    #mp.set_start_method(perform_recognize_faster_whisper,True)
+    dll_load_start = time.time()
+    print(f"Loading CUDA DLL")
     proc.start()
+    print(f"Loaded CUDA DLL ({round(time.time()-dll_load_start,2)}s)")
     result = queue.get()
+    #print(f"Got Whisper Recognizer Process Result ({time.time()})")
     proc.join()
     return result
 
@@ -60,20 +71,18 @@ def perform_recognize_faster_whisper(
     back to the calling stub function with a Queue.
     """
 
-    import numpy as np
-    import soundfile as sf
-    import faster_whisper
-
     start_time = time.time()
 
-    print(f"INIT WHISPER ({start_time})")
-    print(f"STARTING TRANSCRIPTION - model: {model}, download_root: {download_root}, beam_size: {beam_size}, device: {device}, compute_type: {compute_type}, language: {language}, translate: {translate}")
+    #print(f"INIT WHISPER ({start_time})")
+    #print(f"STARTING TRANSCRIPTION - model: {model}, download_root: {download_root}, beam_size: {beam_size}, device: {device}, compute_type: {compute_type}, language: {language}, translate: {translate}")
 
+    # See this discussion on multithreading and multi GPU inference: https://github.com/guillaumekln/faster-whisper/issues/100
     whisper_model = faster_whisper.WhisperModel(model, device=device, compute_type=compute_type, download_root=download_root)
-    print(f"LOADED MODEL ({time.time()})")
+    print(f"LOADED WHISPER MODEL ({round(time.time()-start_time,2)}s)")
+
     # 16 kHz https://github.com/openai/whisper/blob/28769fcfe50755a817ab922a7bc83483159600a9/whisper/audio.py#L98-L99
     wav_bytes = audio_data.get_wav_data(convert_rate=16000)
-    print(f"CONVERTED AUDIO STREAM TO 16Khz ({time.time()})")
+    #print(f"CONVERTED AUDIO STREAM TO 16Khz ({time.time()})")
     wav_stream = BytesIO(wav_bytes)
     audio_array, sampling_rate = sf.read(wav_stream)
     audio_array = audio_array.astype(np.float32)
@@ -87,6 +96,7 @@ def perform_recognize_faster_whisper(
     )
     found_text = list()
     found_tokens = list()
+    # transcribe only runs gpu inference when iterating over the segments
     for segment in segments:
         # segment properties: 'avg_logprob', 'compression_ratio', 'count', 'end', 'id', 'index', 'no_speech_prob', 'seek', 'start', 'temperature', 'text', 'tokens', 'words'
         found_text.append(segment.text)
@@ -94,11 +104,11 @@ def perform_recognize_faster_whisper(
         print(f"Transcript not contain speech?: {segment.no_speech_prob}")
         # TODO: skip output if one or more segments did not contain speech
     text = ' '.join(found_text).strip()
-    print("\n")
+    print("------------------------------------------------------------")
     print(f"FINISHED TRANSCRIPTION ({round(time.time()-start_time, 2)}s): {text}")
     print(f"Tokens: {found_tokens}")
     print(f"Input audio duration: {info.duration_after_vad}s")
-    print("\n")
+    print("------------------------------------------------------------")
     if show_dict:
         result = {
             "text": text,
